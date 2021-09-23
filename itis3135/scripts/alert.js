66459b01179d8b3fe04df46585852d5cf2e9e064
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-09-23 14:47:47 -0400

    Update alert.js

--- itis3135/scripts/alert.js
+++ itis3135/scripts/alert.js
@@ -7,5 +7,7 @@ alert("Hey my script is running")
 var username = prompt("What would you like to be called");
 console.log(username);
 
+document.getElementById().innerHTML ="this is " + username;
+
 var mood = prompt("And how are you feeling today");
 console.log(mood);
