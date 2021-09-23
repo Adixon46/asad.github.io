a947c99e2e873b5e177c87856821b012ee9e4b58
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-09-23 16:29:11 -0400

    Update alert.js

--- itis3135/scripts/alert.js
+++ itis3135/scripts/alert.js
@@ -28,8 +28,9 @@ var time = currentTime.getHours() + ":" + currentTime.getUTCMinutes()
 function maths(){
 
 var Num1 = prompt("First number");
+console.log(Num1);
 var Num2 = prompt("Second number");
-
+console.log(Num2);
 var result = Num1 + Num2;
 return result;
 
