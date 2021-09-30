def52fe4ad88f1b8cc2e7af40da400d73940ae7a
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-09-30 19:11:06 -0400

    Update cal.js

--- itis3135/scripts/cal.js
+++ itis3135/scripts/cal.js
@@ -4,8 +4,6 @@ const calculator = document.querySelector(".calculator")
 
 const keys = calculator.querySelector("cal_keys")
 
-const key = e.target;
-const action = key.dataset.action
 
 keys.addEventListener ("click", e =>{
 
