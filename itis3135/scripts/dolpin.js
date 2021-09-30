cce060de329291c6e845a571aacfcbb123f2482e
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-09-30 16:47:35 -0400

    asa

---
+++ itis3135/scripts/dolpin.js
@@ -0,0 +1,22 @@
+var username = prompt("What would you like to be called");
+console.log(username);
+
+
+
+var mood = prompt("And how are you feeling today");
+console.log(mood);
+
+
+
+var currentDate = new Date();
+    
+var date = (currentDate.getMonth() +1 ) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear();
+
+var currentTime = new Date();
+
+var time = currentTime.getHours() + ":" + currentTime.getMinutes()
+
+
+var dolphin =  document.createElement("img");
+dolphin.src = "images/Dolphin-logo.svg (httpscommons.wikimedia.org).png";
+document.body.appendChild(dolphin)
