797ba865f3dbff7854f98d400a97fd5d1214db80
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-09-30 20:33:51 -0400

    12

--- itis3135/scripts/cal.js
+++ itis3135/scripts/cal.js
@@ -1,5 +1,27 @@
 
 
-const numbers = document.querySelector('[data-number]')
 
-const operations = document.querySelector('[data-opertation]')
+
+class cal{constructor(previousElement, currentElement){
+
+this.previousElement = previousElement;
+this.currentElement = currentElement;
+this.clear();
+
+}    }
+
+const numbersChoice = document.querySelectorAll('[data-number]')
+
+const operationsChoice = document.querySelectorAll('[data-opertation]')
+
+const equalsChoice = document.querySelector('[data-equals]')
+
+const deleteChoice = document.querySelector('[data-equals]')
+
+const clearChoice = document.querySelector('[data-clear-op]')
+
+const previousChoice = document.querySelector('[data-previous-op]')
+
+const currentChoice = document.querySelector('[data-current-op]')
+
+
