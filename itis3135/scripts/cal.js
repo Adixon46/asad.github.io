24a919287cd42772949e8118862761458736ea2f
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-10-01 11:16:10 -0400

    Update cal.js

--- itis3135/scripts/cal.js
+++ itis3135/scripts/cal.js
@@ -1,15 +1,4 @@
 
-
-
-
-class cal{constructor(previousElement, currentElement){
-
-this.previousElement = previousElement;
-this.currentElement = currentElement;
-this.clear();
-
-}    }
-
 const numbersChoice = document.querySelectorAll('[data-number]')
 
 const operationsChoice = document.querySelectorAll('[data-opertation]')
@@ -24,4 +13,79 @@ const previousChoice = document.querySelector('[data-previous-op]')
 
 const currentChoice = document.querySelector('[data-current-op]')
 
+const calculator = new cal(previousChoice, currentChoice){
 
+
+
+
+}
+
+
+class cal{constructor(previousChoice, currentChoice){
+
+this.previousChoice = previousChoice;
+this.currentChoice = currentChoice;
+this.clear();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+clear(){
+
+    this.currentOp = ''
+    this.previousOP = ''
+    this.operation = undefined
+
+}
+
+delete(){
+
+
+
+}
+
+append(){
+numbersChoice.forEach(button => {
+button.addEventListener('click' , () => {
+cal.append(button.innerText)
+cal.refreshDisplay()
+
+
+
+})
+
+})
+
+
+}
+
+chooseOp(){
+
+
+}
+
+
+compute(){
+
+
+}
+
+
+refreshDisplay(){
+
+
+
+}
+
+}
