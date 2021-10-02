e4a6f10809c15ef934459cf0e1dfbe1ed0144eef
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-10-02 12:05:20 -0400

    Update cal.js

--- itis3135/scripts/cal.js
+++ itis3135/scripts/cal.js
@@ -1,11 +1,24 @@
 
+const numbersButton = document.querySelectorAll('[data-number]')
+
+const operationsButton = document.querySelectorAll('[data-opertation]')
+
+const equalsButton = document.querySelector('[data-equals]')
+
+const deleteButton = document.querySelector('[data-delete]')
+
+const clearButton = document.querySelector('[data-clear]')
+
+const previousOperation = document.querySelector('[data-previous]')
+
+const currentOperation = document.querySelector('[data-current]')
 
 class Calculator{
 
-constructor(previousOperationTextELement, currentOperationTextElement){
+constructor(previousOperation,currentOperation){
 
-this.previousOperationTextELement = previousOperationTextELement
-this.currentOperationTextElement = currentOperationTextElement
+this.previousOperation = previousOperation
+this.currentOperation = currentOperation
 this.clear()
 
     }
@@ -48,31 +61,22 @@ calculate(){
 refreshDisplay(){
 
 
-this.currentOperationTextElement.innerText = this.current
+this.currentOperation.innerText = this.current
 }
 
 
+
+
+
+
 }
 
 
 
 
 
-const numbersButton = document.querySelectorAll('[data-number]')
 
-const operationsButton = document.querySelectorAll('[data-opertation]')
-
-const equalsButton = document.querySelector('[data-equals]')
-
-const deleteButton = document.querySelector('[data-delete]')
-
-const clearButton = document.querySelector('[data-clear]')
-
-const previousOperationTextELement = document.querySelector('[data-previous]')
-
-const currentOperationTextElement = document.querySelector('[data-current]')
-
-const cal = new Calculator(previousOperationTextELement ,currentOperationTextElement)
+const cal = new Calculator(previousOperation ,currentOperation)
 
 numbersButton.forEach(button =>{
     button.addEventListener('click' , () =>{
