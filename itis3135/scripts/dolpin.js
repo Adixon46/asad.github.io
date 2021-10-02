a1c1e1c836526e26149f6694d077a80ea33a032f
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-10-02 16:16:14 -0400

    qw

--- itis3135/scripts/dolpin.js
+++ itis3135/scripts/dolpin.js
@@ -9,14 +9,14 @@ console.log(mood);
 
 
 var currentDate = new Date();
-    
-var date = (currentDate.getMonth() +1 ) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear();
+
+var date = (currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear();
 
 var currentTime = new Date();
 
 var time = currentTime.getHours() + ":" + currentTime.getMinutes()
 
 
-var dolphin =  document.createElement("img");
+var dolphin = document.createElement("img");
 dolphin.src = "images/Dolphin-logo.svg (httpscommons.wikimedia.org).png";
 document.body.appendChild(dolphin)
