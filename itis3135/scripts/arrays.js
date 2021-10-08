ed681271dd14d0d05243d1a9adb9bb9961ac77b6
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-10-07 22:10:03 -0400

    12

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -26,28 +26,10 @@ function displayResults(){
 
 function displaySalary(){
 
-    //document.getElementById("name").innerHTML = person;
+    document.getElementById("name").innerHTML = person;
 
-//document.getElementById("money").innerHTML = salaries;
+document.getElementById("money").innerHTML = salaries;
    
-var table = document.getElementById("results_table");
 
-for(var i = 0; i < table.rows.length; i++)
-{
-
-    
-
-
-table.innerHTML = person[i];
-
-}
-
-
-    for(var j = 0; j < table.length; j++)
-{
-    
-    
-    table.innerHTML = salaries[j];
-}
     
 } 
