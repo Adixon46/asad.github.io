fa6a8ad8e89e7f05c164316c3c343d31c6c001b1
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-10-07 23:21:35 -0400

    Update arrays.js

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -15,7 +15,7 @@ console.log(salaries)
 
 var select = document.getElementById("sel");
 var option = document.createElement("option")
-option.text = person[0];
+option.text = person[person.pop];
 select.add(option)
 
 
