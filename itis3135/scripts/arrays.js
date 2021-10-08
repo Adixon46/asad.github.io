485d9d3e87a1aced269f61c445df3058c3fb2a1d
[adixon46] <89495229+Adixon46@users.noreply.github.com>
2021-10-07 21:24:43 -0400

    Update arrays.js

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -21,37 +21,33 @@ function displayResults(){
 
   }  
 
-document.getElementById("name").innerHTML = person;
+
+
+
+function displaySalary(){
+
+    document.getElementById("name").innerHTML = person;
 
 document.getElementById("money").innerHTML = salaries;
    
 var table = document.getElementById("results_table");
 
 for(var i = 0; i < results_table.rows.length; i++)
-
 {
+
+
+
 
 results_table.rows.innerHTML = person[i];
 
+}
 
-{
 
     for(var j = 0; j < results_table.rows.length; j++)
-
-    {
+{
+    
     
     results_table.rows.innerHTML = salaries[j];
+}
     
-    
-    {
-
-}
-}
-
-
-function displaySalary(){
-
-    document.getElementById("displayS").innerHTML = "Hello World";
-
-
-}
+} 
